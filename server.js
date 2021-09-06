995fea573da780cedd76e10985ae27cece55826b
Rachel Gregg <rgregg2014@gmail.com>
2021-09-06 18:51:43 -0400

    added front end routes, styled pages

--- server.js
+++ server.js
@@ -2,20 +2,20 @@ const path = require("path");
 const express = require("express");
 const expbhs = require("express-handlebars");
 
-// const routes = require("./controllers");
-// const sequelize = require("./config/connection");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const hbs = expbhs.create({});
+const hbs = expbhs.create({});
 
-// app.engine("handlebars", hbs.engine);
-// app.set("view engine", "handlebars");
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 // app.use(routes);
 
@@ -24,5 +24,17 @@ app.listen(PORT, () =>
 );
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard");
 });
